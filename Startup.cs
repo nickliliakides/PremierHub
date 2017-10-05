c812aa1b463288dc8d5697d0e6fce47bf2d1869c
Nikolaos Liliakidis <nickliliakides@gmail.com>
2017-10-05 12:18:17 +0100

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PremierHub.Startup))]
+namespace PremierHub
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
